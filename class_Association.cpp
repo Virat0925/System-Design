774e9fced8f880843ccbf281200919ce2e0ed528
Virat0925 <greatvirat0925@gmail.com>
2025-10-29 23:58:27 +0530

    commit

--- class_Association.cpp
+++ class_Association.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Example of Structural Class UML diagram
 class Car{
     private:
         string model;
@@ -22,12 +23,41 @@ public:
     };
 };
 
+// Example of Structural Class UML diagram
+
+class Room{
+
+    public:
+
+    void canSleep(){
+        cout << "We can Sleep inside" << endl;
+    }
+};
+
+class Sofa{
+
+    Room *livingRoom;
+
+    Sofa(){
+        livingRoom = new Room;
+    }
+
+    void canRest(){
+        cout << "Resting" << endl;
+    }
+};
+
 int main(){
     manualCar *Tesla = new manualCar();
 
     Tesla->startEngine();
     Tesla->gearShift();
 
+    Sofa *chair = new Sofa;
+
+    
+
     delete Tesla;
     return 0;
 }
+
